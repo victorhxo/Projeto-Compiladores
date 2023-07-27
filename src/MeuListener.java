c26eb0176f4d522be5e6d9f265b849b0e955306a
victorhxo <victor_hx@hotmail.com>
2023-07-27 17:46:22 -0300

    Verificação de tipos

--- src/MeuListener.java
+++ src/MeuListener.java
@@ -1,7 +1,7 @@
-import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import java.util.*;
 
 public class MeuListener extends PyCBaseListener{
 
@@ -19,7 +19,7 @@ public class MeuListener extends PyCBaseListener{
         String tipo = ctx.TIPO().getText();
         String id = ctx.ID().getText();
         if (tabelaSimbolos.containsKey(id)){
-            System.out.println("Declaracao duplicada! Variavel " + id + " ja declarada");
+            System.out.println("Erro: Declaracao duplicada!" + "\n" + "Variavel [" + id + "] ja declarada");
         } else {
             tabelaSimbolos.put(id,tipo);
         }
@@ -34,12 +34,48 @@ public class MeuListener extends PyCBaseListener{
         }
     }
 
+    @Override
+    public void enterNEscrita(PyCParser.NEscritaContext ctx) {
+        String id = ctx.ID().getText();
+        if (!tabelaSimbolos.containsKey(id)){
+            tabelaUsosNaoDeclarados.add(id);
+        }
+    }
+
+    @Override
+    public void enterNLeitura(PyCParser.NLeituraContext ctx) {
+        String id = ctx.ID().getText();
+        if (!tabelaSimbolos.containsKey(id)){
+            tabelaUsosNaoDeclarados.add(id);
+        }
+    }
 
     @Override
     public void enterNAtribuicao(PyCParser.NAtribuicaoContext ctx) {
-        String id = ctx.ID(0).getText();
-        if (!tabelaSimbolos.containsKey(id)) {
-            tabelaUsosNaoDeclarados.add(id);
+        List<String> ids = new ArrayList<>();
+        for (TerminalNode idNode : ctx.ID()) {
+            String id = idNode.getText();
+            if (!tabelaSimbolos.containsKey(id)){
+                tabelaUsosNaoDeclarados.add(id);
+            }
+            ids.add(id);
+        }
+
+        if(ids.size() == 2){
+            String id1 = ids.get(0);
+            String id2 = ids.get(1);
+
+            if (tabelaSimbolos.containsKey(id1) && tabelaSimbolos.containsKey(id2)) {
+                String tipo1 = tabelaSimbolos.get(id1);
+                String tipo2 = tabelaSimbolos.get(id2);
+
+                if (!tipo1.equals(tipo2)) {
+                    int linha = ctx.getStart().getLine();
+                    System.out.println("Erro: Tipos dos IDs em atribuição não são iguais na linha de numero " + linha);
+                    System.out.println("ID: [" + id1 + "] tem tipo [" + tipo1 + "]");
+                    System.out.println("ID: [" + id2 + "] tem tipo [" + tipo2 + "]");
+                }
+            }
         }
     }
 
